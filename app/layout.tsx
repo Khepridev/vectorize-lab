e1e6b79af7e0960c956c8cfa12e9fe7618ac6b8e
Khepridev <khepri0@proton.me>
2026-05-05 04:17:18 +0300

    add vercel speed insights integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { cookies } from "next/headers";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
@@ -40,6 +41,7 @@ export default async function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
